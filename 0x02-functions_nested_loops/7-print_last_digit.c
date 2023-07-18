e6661a9a2e8a93d18c17a6e23fab2e51d606f1b9
SandileAlmighty <khumalosandile04@gmail.com>
2023-07-18 09:11:03 -0700

    digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,18 +14,17 @@
  */
 int print_last_digit(int n)
 {
-	int a;
+	int last_digit;
 
 	if (n < 0)
 		n = -n;
 
-	a = n % 10;
+	last_digit = n % 10;
 
-	if (a < 0)
-		a = -a;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-	_putchar(a + '0');
+	_putchar(last_digit + '0');
 
-	return (a);
+	return (last_digit);
 }
-
