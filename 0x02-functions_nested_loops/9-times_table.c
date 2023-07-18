e554edc90d4efd858f63a474cbdec2095f89ec94
SandileAlmighty <khumalosandile04@gmail.com>
2023-07-18 09:21:03 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,16 @@
 #include "main.h"
 
-
-	/**
-	 * jack_bauer - prints every minute of the day of Jack Bauer
-	 * starting from 00:00 to 23:59
-	 */
-	void jack_bauer(void)
-	{
-		int i, j;
-
-
-		i = 0;
-
-
-		while (i < 24)
-		{
-			j = 0;
-			while (j < 60)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-				j++;
-			}
-			i++;
-		}
-	}
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ *
+ * Description: This function serves as the entry point of the program.
+ *              It calls the times_table function to generate and print
+ *              the times table. The function then returns 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
