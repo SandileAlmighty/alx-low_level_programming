96dc940d0adc121d04ec6629a23650fd630601a4
SandileAlmighty <khumalosandile04@gmail.com>
2023-07-18 09:26:12 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,12 @@
+
 #include "main.h"
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- *
- * Description: This function serves as the entry point of the program.
- *              It calls the times_table function to generate and print
- *              the times table. The function then returns 0.
- */
+
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
 	int main(void)
 	{
 	    times_table();
